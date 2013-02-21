72ce3c61ea8ddee620cd3bfbceda92b7f8afdaef
Sijawusz Pur Rahnama <sija@sija.pl>
2013-02-21 02:33:37 +0100

    Revert usage of block form of `Net::HTTP.start`
    - Switched to `Net::HTTP.new` as `Net::HTTP.start` doesn't honor timeout values (http://www.ruby-forum.com/topic/148968)
    - Closes #16, fixes #20, and probably fixes #15

--- test/unit/garb/request/data_test.rb
+++ test/unit/garb/request/data_test.rb
@@ -121,18 +121,20 @@ module Garb
           http = mock do |m|
             m.expects(:open_timeout=).with(Garb.open_timeout)
             m.expects(:read_timeout=).with(Garb.read_timeout)
-            m.expects(:get).with('/data?key=value', {
+            m.expects(:use_ssl=).with(true)
+            m.expects(:verify_mode=).with(OpenSSL::SSL::VERIFY_NONE)
+            m.expects(:request_get).with('/data?key=value', {
               'Authorization' => 'GoogleLogin auth=toke',
               'GData-Version' => '3'
-            })
+            }).returns(response)
           end
 
           Garb.proxy_address = '127.0.0.1'
           Garb.proxy_port = '1234'
-          Net::HTTP.expects(:start).with('example.com', 443, '127.0.0.1', '1234', :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE).yields(http).returns(response)
+          Net::HTTP.expects(:new).with('example.com', 443, '127.0.0.1', '1234').returns(http)
 
           data_request = Request::Data.new(@session, 'https://example.com/data', 'key' => 'value')
-          assert_equal response, data_request.single_user_request
+          data_request.single_user_request
         end
 
         should "merge :api_key into the parameters" do
